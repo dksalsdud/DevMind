572f66ce82cbcba1d39d8d5aff73477d378c81db
dksalsdud <dkslasdud@nsu.ac.kr>
2025-12-06 13:29:17 +0000

    [feat] 초기 세팅

---
+++ DevMind/src/main/java/com/example/DevMind/DevMindApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevMindApplication.class, args);
+	}
+
+}
