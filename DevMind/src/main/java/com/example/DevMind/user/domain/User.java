a9be7acf72a67fbd30f9ed8ad95056b149f27686
dksalsdud <dkslasdud@nsu.ac.kr>
2025-12-07 13:20:00 +0000

    [feat] id PK 추가

--- DevMind/src/main/java/com/example/DevMind/user/domain/User.java
+++ DevMind/src/main/java/com/example/DevMind/user/domain/User.java
@@ -1,6 +1,9 @@
 package com.example.DevMind.user.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,5 +17,9 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
 }
