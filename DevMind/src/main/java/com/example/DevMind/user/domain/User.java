fe0d334a09c14b7c3b34d7289956ab04915e9555
dksalsdud <dkslasdud@nsu.ac.kr>
2025-12-07 13:14:20 +0000

    [feat] Getter Setter 어노테이션 추가

--- DevMind/src/main/java/com/example/DevMind/user/domain/User.java
+++ DevMind/src/main/java/com/example/DevMind/user/domain/User.java
@@ -1,8 +1,12 @@
 package com.example.DevMind.user.domain;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class User {
     
 }
