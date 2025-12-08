8f46111ef51379cfbeee57baacd746c719c277c1
dksalsdud <dkslasdud@nsu.ac.kr>
2025-12-08 13:37:13 +0000

    [test] User 기본 생성자 테스트
    객체가 생성되었는지 확인
    
    객체가 DB에 저장이 안되어있기 때문에 유저객체 아이디가 null이 맞는지 확인하는 테스트
    
    테스트 진행 결과 통과

--- DevMind/src/test/java/com/example/DevMind/user/domain/UserTest.java
+++ DevMind/src/test/java/com/example/DevMind/user/domain/UserTest.java
@@ -1,6 +1,8 @@
 package com.example.DevMind.user.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.time.LocalDateTime;
 
@@ -12,7 +14,7 @@ public class UserTest {
     @Test
     @DisplayName("User 객체 생성 및 빌더 패턴 테스트")
     void create_user_test() {
-        
+
         // given
         String email = "test@example.com";
         String nickname = "Tester";
@@ -40,4 +42,15 @@ public class UserTest {
         assertEquals(now, user.getCreated_at());
     }
 
+    @Test
+    @DisplayName("User 기본 생성자 테스트")
+    void default_constructor_test() {
+
+        // when
+        User user = new User();
+
+        // then
+        assertNotNull(user); // 객체가 생성 되었는지 확인
+        assertNull(user.getId()); // ID는 DB 저장 시 생성되므로 null이어야 함
+    }
 }
