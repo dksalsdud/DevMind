9b2ed887554d4ffe12b546d8c2ab01129075f7f1
dksalsdud <dkslasdud@nsu.ac.kr>
2025-12-08 13:32:51 +0000

    [test] User 객체 생성 및 빌더 패턴 테스트
    유저 객처를 생성 한 다음 빌더 패턴을 통해 유저를 생성한다.
    
    assertEquals 함수를 통해 각각의 유저 객체의 기대값과 실제값을 비교하여 테스트 진행 결과 통과

--- DevMind/src/test/java/com/example/DevMind/user/domain/UserTest.java
+++ DevMind/src/test/java/com/example/DevMind/user/domain/UserTest.java
@@ -1,5 +1,43 @@
 package com.example.DevMind.user.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class UserTest {
     
+    @Test
+    @DisplayName("User 객체 생성 및 빌더 패턴 테스트")
+    void create_user_test() {
+        
+        // given
+        String email = "test@example.com";
+        String nickname = "Tester";
+        String name = "Test User";
+        String socialProvider = "GOOGLE";
+        UserRole role = UserRole.ROLE_USER;
+        LocalDateTime now = LocalDateTime.now();
+
+        // when
+        User user = User.builder()
+                .email(email)
+                .nickname(nickname)
+                .name(name)
+                .social_provider(socialProvider)
+                .role(role)
+                .created_at(now)
+                .build();
+
+        // then: assertEquals(기대값, 실제값) 순서
+        assertEquals(email, user.getEmail());
+        assertEquals(nickname, user.getNickname());
+        assertEquals(name, user.getName());
+        assertEquals(socialProvider, user.getSocial_provider());
+        assertEquals(role, user.getRole());
+        assertEquals(now, user.getCreated_at());
+    }
+
 }
