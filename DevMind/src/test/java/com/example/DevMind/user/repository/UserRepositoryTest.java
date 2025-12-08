325d29c829dfdd8db3d2d0153187cf6878503b02
dksalsdud <dkslasdud@nsu.ac.kr>
2025-12-08 13:47:45 +0000

    [test] User 저장 및 조회 테스트
    유저 저장 후 PK ID값 생성 되었는지 확인
    저장된 유저 객채의 이메일과 예측되는 이메일이 같은지 확인
    
    ID로 다시 조회 후 조회된 유저 객체 닉네임과 예측되는 닉네임이 같은지 테스트 진행
    
    결과 통과

--- DevMind/src/test/java/com/example/DevMind/user/repository/UserRepositoryTest.java
+++ DevMind/src/test/java/com/example/DevMind/user/repository/UserRepositoryTest.java
@@ -1,12 +1,50 @@
 package com.example.DevMind.user.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import com.example.DevMind.user.domain.User;
+import com.example.DevMind.user.domain.UserRole;
+import com.example.DevMind.user.reository.UserRepository;
 
 @DataJpaTest
 public class UserRepositoryTest {
     
     @Autowired
     private UserRepository userRepository;
-    
+
+    @Test
+    @DisplayName("User 저장 및 조회 테스트")
+    void save_and_find_user() {
+        
+        // given
+        User user = User.builder()
+                .email("save@test.com")
+                .nickname("SaveNick")
+                .name("SaveName")
+                .social_provider("NAVER")
+                .created_at(LocalDateTime.now())
+                .role(UserRole.ROLE_USER)
+                .build();
+
+        // when
+        User savedUser = userRepository.save(user);
+
+        // then
+        assertNotNull(savedUser.getId()); // ID 자동 생성 확인
+        assertEquals("save@test.com", savedUser.getEmail()); // 이메일 맞는지 확인
+        
+        // ID로 다시 조회 확인
+        User foundUser = userRepository.findById(savedUser.getId())
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        
+        assertEquals("SaveNick", foundUser.getNickname());
+    }
 }
