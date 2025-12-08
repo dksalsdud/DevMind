b2671acf5921b2647973d60c50e4047d472d2046
dksalsdud <dkslasdud@nsu.ac.kr>
2025-12-08 13:39:30 +0000

    [test] 의존성 추가 어노테이션 추가

--- DevMind/src/test/java/com/example/DevMind/user/repository/UserRepositoryTest.java
+++ DevMind/src/test/java/com/example/DevMind/user/repository/UserRepositoryTest.java
@@ -1,5 +1,12 @@
 package com.example.DevMind.user.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+@DataJpaTest
 public class UserRepositoryTest {
+    
+    @Autowired
+    private UserRepository userRepository;
     
 }
