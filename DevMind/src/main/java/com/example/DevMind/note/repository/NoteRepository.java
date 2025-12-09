7cb40b58468268ef93ffbba2f020049fce04a89b
dksalsdud <dkslasdud@nsu.ac.kr>
2025-12-09 14:25:14 +0000

    [feat] extends JpaRepository 추가

--- DevMind/src/main/java/com/example/DevMind/note/repository/NoteRepository.java
+++ DevMind/src/main/java/com/example/DevMind/note/repository/NoteRepository.java
@@ -1,8 +1,11 @@
 package com.example.DevMind.note.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.DevMind.note.domain.Note;
+
 @Repository
-public interface NoteRepository {
+public interface NoteRepository extends JpaRepository<Note, Long> {
     
 }
