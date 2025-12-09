e1d79180f000ddd5e1c64a029123bd52447d87ac
dksalsdud <dkslasdud@nsu.ac.kr>
2025-12-09 13:17:55 +0000

    [feat] @Getter @Setter 어노테이션 추가

--- DevMind/src/main/java/com/example/DevMind/note/domain/Note.java
+++ DevMind/src/main/java/com/example/DevMind/note/domain/Note.java
@@ -1,8 +1,12 @@
 package com.example.DevMind.note.domain;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Note {
     
 }
