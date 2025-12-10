993c1c7510de0936a8e2c0f3ad379805d61b6e9b
dksalsdud <dkslasdud@nsu.ac.kr>
2025-12-10 13:23:44 +0000

    [test] Note 객체 빌더 패턴 생성 테스트
    노트 앤티티 객체를 빌더 패턴을 통해 생성
    
    생성된 객체가 갖는 값과 주어진 값이 같은지 테스트
    
    결과 통과

--- DevMind/src/test/java/com/example/DevMind/note/domain/NoteTest.java
+++ DevMind/src/test/java/com/example/DevMind/note/domain/NoteTest.java
@@ -1,5 +1,36 @@
 package com.example.DevMind.note.domain;
 
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class NoteTest {
     
+    @Test
+    @DisplayName("Note 객체 빌더 패턴 생성 테스트")
+    void createNoteUsingBuilder() {
+        // given
+        String title = "Test Title";
+        String content = "Test Content";
+        SourceType sourceType = SourceType.INTERNAL;
+        LocalDateTime now = LocalDateTime.now();
+
+        // when
+        Note note = Note.builder()
+                .title(title)
+                .content(content)
+                .summary("Summary")
+                .source_type(sourceType)
+                .created_at(now)
+                .build();
+
+        // then
+        assertThat(note.getTitle()).isEqualTo(title);
+        assertThat(note.getContent()).isEqualTo(content);
+        assertThat(note.getSource_type()).isEqualTo(sourceType);
+        assertThat(note.getCreated_at()).isEqualTo(now);
+    }
 }
