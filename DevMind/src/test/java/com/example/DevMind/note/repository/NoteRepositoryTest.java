84e51ef4c29d175337c30aa67b6469efd98f377d
dksalsdud <dkslasdud@nsu.ac.kr>
2025-12-10 13:37:03 +0000

    [test] DataJpaTest AutoConfigureTestDatabase 어노테이션 추가

--- DevMind/src/test/java/com/example/DevMind/note/repository/NoteRepositoryTest.java
+++ DevMind/src/test/java/com/example/DevMind/note/repository/NoteRepositoryTest.java
@@ -1,5 +1,10 @@
 package com.example.DevMind.note.repository;
 
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // 실제 DB 사용
 public class NoteRepositoryTest {
     
 }
